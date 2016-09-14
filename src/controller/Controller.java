9d86b8b0ce8816b89df8a5563ec2a0ca44123087
Fredrik Olsson <folep02@student.lnu.se>
2016-09-14 12:17:12 +0200

    add user light...

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -6,7 +6,9 @@ public class Controller {
     public void Show(Console a_view) {
 
         int menuNumber = a_view.presentMainMenu();
-
+        if(menuNumber == 1) {
+            a_view.presentAddUser();
+        }
 
 
 
