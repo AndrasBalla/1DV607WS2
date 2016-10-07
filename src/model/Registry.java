8d3bce94774f90a490abbdda43e85f5da052ddcd
Fredrik Olsson <folep02@student.lnu.se>
2016-10-07 10:58:26 +0200

    comments, xml-error

--- src/model/Registry.java
+++ src/model/Registry.java
@@ -6,7 +6,7 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement(name = "data")
+@XmlRootElement(name = "Registry")
 public class Registry {
     private ArrayList<Member> members = new ArrayList<Member>();
     private int count = 0;
